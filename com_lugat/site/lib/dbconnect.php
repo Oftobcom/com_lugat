95df5a80a27a082a6eb5f08acb4c0af8dded91d3
oftobcom <oftob.com>
2014-03-27 21:11:45 +0500

    delete files

--- com_lugat/site/lib/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//соединеняемся с сервером MySQL
-$conn=mysql_connect("localhost","root","root") or die("Невозможно установить соединение");
-mysql_select_db("lugat_db") or die("Невозможно открыть базу данных"); //открываем базу данных
-mysql_query("SET NAMES utf8"); //настройка кодировки MySQL
-?>
-
